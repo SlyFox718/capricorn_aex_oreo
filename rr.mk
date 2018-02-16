5b745ae9ce9fe463d368feebc6f02b854b2d5949
Egor Ermakov <typoumdk@gmail.com>
2018-02-16 13:53:51 +0500

    will try again(

--- rr.mk
+++ rr.mk
@@ -17,4 +17,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys"
 
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.rr.version=Unofficial by TrOLOLO718
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
